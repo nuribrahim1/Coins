ef02e38b15a02599e0c8b8e9b86e10f1880d28c7
nuribrahim1 <nuribrahim1@outlook.com>
2026-02-07 23:06:23 +0000

    added plot to test

--- ingestion/test_client.py
+++ ingestion/test_client.py
@@ -1,4 +1,7 @@
 from ingestion.coingecko_client import CoinGeckoClient
+from datetime import datetime
+import matplotlib.pyplot as plt
+
 
 client = CoinGeckoClient()
 
@@ -6,4 +9,20 @@ markets = client.get_markets()
 print(markets[0])
 
 chart = client.get_market_chart("bitcoin", days=7)
-print(chart)
+
+
+coordinates = chart['prices']
+
+print(coordinates)
+X = []
+y = []
+
+for i in coordinates:
+    X.append(datetime.fromtimestamp(i[0]/1000))
+    y.append(round(i[1],2))
+
+plt.plot(X,y)
+plt.show()
+
+
+
